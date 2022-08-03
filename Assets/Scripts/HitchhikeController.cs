9fda88b74222faf4b50c35d72946be6c384a8cc7
CyberLab PC 9 <cyber@cyber.t.u-tokyo.ac.jp>
2022-08-03 19:14:22 +0900

    eye working

--- Assets/Scripts/HitchhikeController.cs
+++ Assets/Scripts/HitchhikeController.cs
@@ -14,18 +14,7 @@ namespace ViveSR
       {
         public GameObject target;
         public GameObject headOrigin;
-        EyeData eye;
-        Vector3 CombineGazeRayorigin; // left and right eyes combined gaze
-        Vector3 CombineGazeRaydirection;
-        Ray CombineRay;
-        /*レイがどこに焦点を合わせたかの情報．Vector3 point : 視線ベクトルと物体の衝突位置，float distance : 見ている物体までの距離，
-                   Vector3 normal:見ている物体の面の法線ベクトル，Collider collider : 衝突したオブジェクトのCollider，Rigidbody rigidbody：衝突したオブジェクトのRigidbody，Transform transform：衝突したオブジェクトのTransform*/
-        //焦点位置にオブジェクトを出すためにpublicにしています．
-        public static FocusInfo CombineFocus;
-        //レイの半径
-        float CombineFocusradius;
-        //レイの最大の長さ
-        float CombineFocusmaxDistance;
+
         // Start is called before the first frame update
         void Start()
         {
@@ -35,21 +24,38 @@ namespace ViveSR
         // Update is called once per frame
         void Update()
         {
-          if (SRanipal_Eye_API.GetEyeData(ref eye) != ViveSR.Error.WORK)
+          if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING)
           {
             Debug.Log("Eye tracking malfunctioning");
             return;
           }
 
-          SRanipal_Eye_API.GetEyeData(ref eye);
-          if (SRanipal_Eye.GetGazeRay(GazeIndex.COMBINE, out CombineGazeRayorigin, out CombineGazeRaydirection, eye))
+          var gazeRay = GetGazeRay();
+
+          if (Physics.Raycast(gazeRay, out var hit))
           {
-            Debug.Log("COMBINE GazeRayorigin" + CombineGazeRayorigin.x + ", " + CombineGazeRayorigin.y + ", " + CombineGazeRayorigin.z);
-            Debug.Log("COMBINE GazeRaydirection" + CombineGazeRaydirection.x + ", " + CombineGazeRaydirection.y + ", " + CombineGazeRaydirection.z);
-            Debug.Log(CombineGazeRaydirection.magnitude);
-            target.transform.position = headOrigin.transform.position + CombineGazeRayorigin;
-            target.transform.position += CombineGazeRaydirection;
+            target.SetActive(true);
+            target.transform.position = hit.point;
           }
+          else
+          {
+            target.SetActive(false);
+          }
+        }
+
+        private Ray GetGazeRay()
+        {
+          var eyeData = GetCombinedSingleEyeData();
+          var gazeDirection = eyeData.gaze_direction_normalized;
+          gazeDirection.x *= -1; // right hand coor to left hand coor
+
+          return new Ray(headOrigin.transform.position, headOrigin.transform.rotation * gazeDirection);
+        }
+
+        private SingleEyeData GetCombinedSingleEyeData()
+        {
+          var eyeData = SRanipal_Eye_v2.GetVerboseData(out var verboseData);
+          return verboseData.combined.eye_data;
         }
       }
 
